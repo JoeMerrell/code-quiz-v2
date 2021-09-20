2dc395f6ae760e0ed34911c6f3108d43c0484aba
Joseph <joe@uhhuhohyeah.com>
2021-09-19 17:47:54 -0700

    added questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,123 @@
+// QUESTION OBJECTS
+let questions = [
+    {
+    num: 1,
+    question: "What does HTML mean?",
+    answer: "Hypertext Markup Language",
+    options: [
+      "Hero Text Master Lingo",
+      "Hypertextual Maximus Linguistica",
+      "Hypertext Markup Language",
+      "Historic Timed Mistaken Lexicon"
+    ]
+
+  },
+    {
+    num: 2,
+    question: "What is an API?",
+    answer: "Application Programming Interface",
+    options: [
+      "Avuncular Piecemeal Instruction",
+      "Application Programming Interface",
+      "Application Program Interrogation",
+      "Average Programmatical Interest"
+    ]
+
+  },
+  {
+    num: 3,
+    question: "What is CSS?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Corrective Sheet Syling",
+      "Cascading Style Sheet",
+      "Computational Sync Status",
+      "Cascades of Sheet Style"
+    ]
+
+  },
+    {
+    num: 4,
+    question: "Arrays in JavaScript can store:",
+    answer: "All of the above",
+    options: [
+      "Strings",
+      "Integers",
+      "Booleans",
+      "All of the above"
+    ]
+
+  },
+    {
+    num: 5,
+    question: "What is a function that prints a message to log on the debugging console?",
+    answer: "console.log()",
+    options: [
+      "log.console(message)",
+      "console.log()",
+      "function(log)",
+      "=> && i++"
+    ]
+  },
+  
+  {
+    num: 6,
+    question: "Commonly used data types DO NOT include:",
+    answer: "Alerts",
+    options: [
+      "Booleans",
+      "Alerts",
+      "Numbers",
+      "Strings"
+    ]
+  },
+  
+  {
+    num: 7,
+    question: "What is jQuery?",
+    answer: "A widely-used JavaScript library",
+    options: [
+      "A widely-used JavaScript library",
+      "A FAQ site that provides answers to common Javascript Questions",
+      "A popular manual for JavaScript programmers",
+      "No clue"
+    ]
+  },
+  
+  {
+    num: 8,
+    question: "Apart from integers, another form of numerical data:",
+    answer: "Floating point",
+    options: [
+      "Inversion",
+      "Cavalcade",
+      "Floating point",
+      "Fractionated"
+    ]
+  },
+  
+  {
+    num: 9,
+    question: "Which of these is NOT a programming language?",
+    answer: "Apple",
+    options: [
+      "Ruby",
+      "Java",
+      "Python",
+      "Apple"
+    ]
+  },
+
+  {
+    num: 10,
+    question: "Which of these CSS frameworks was a Twitter initiative?",
+    answer: "Bootstrap",
+    options: [
+      "Materialize",
+      "Bootstrap",
+      "Semantic UI",
+      "Bulma"
+    ]
+  },
+  
+];
